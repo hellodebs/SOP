9779adfdd32241a1c69ff816b013f261f83865f6
hellodebs <debora.behrens@outlook.com>
2021-08-09 12:09:46 +0200

    Npm i, install react-router-dom, add Router to index,  vercel, create four components, add Navlink

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, NavLink } from "react-router-dom";
+import Menu from "./components/Menu";
+import Order from "./components/Order.js";
+import Bill from "./components/Bill.js";
+import Service from "./components/Service.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <Route path="/menu">
+          <Menu />
+        </Route>
+        <Route path="/order">
+          <Order />
+        </Route>
+        <Route path="/bill">
+          <Bill />
+        </Route>
+        <Route path="/service">
+          <Service />
+        </Route>
       </header>
+      <main className="main">
+        <p>blabla</p>
+      </main>
+      <footer className="footer">
+        <nav className="nav">
+          <NavLink to="/menu">Menu</NavLink>
+          <NavLink to="/order">Order</NavLink>
+          <NavLink to="/bill">Bill</NavLink>
+          <NavLink to="/service">Service</NavLink>
+        </nav>
+      </footer>
     </div>
   );
 }
