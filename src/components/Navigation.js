deee2bf53807b0b3aeae7259c977940aee37e68e
hellodebs <debora.behrens@outlook.com>
2021-08-12 17:43:11 +0200

    Make active icons in footer change colors

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,10 +6,18 @@ import { IoHelpBuoyOutline } from "react-icons/io5";
 export default function Navigation() {
   return (
     <nav className="Navigation__nav">
-      <NavLink className="Navigation__navlink" to="/menu">
+      <NavLink
+        className="Navigation__navlink"
+        activeClassName="Navigation__navlink--active"
+        to="/menu"
+      >
         <BiFoodMenu className="Navigation__icon" />
       </NavLink>
-      <NavLink className="Navigation__navlink" to="/order">
+      <NavLink
+        className="Navigation__navlink"
+        activeClassName="Navigation__navlink--active"
+        to="/order"
+      >
         <BiCart className="Navigation__icon" />
       </NavLink>
       <NavLink className="Navigation__navlink" to="/bill">
