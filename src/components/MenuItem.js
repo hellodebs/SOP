994e6f062297194bf4e5ecabfff2b504713f9e3a
hellodebs <debora.behrens@outlook.com>
2021-08-12 12:07:41 +0200

    Create custom hook useCount, export custom hook to MenuItem, create buttons, style a little

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,12 +1,22 @@
-import "../pages/Menu.css";
+import "./MenuItem.css";
+import useCount from "../hooks/useCount";
 
 export default function MenuItem({ item }) {
+  const [count, increment, decrement] = useCount();
   return (
     <div className="menu__item">
       <p>{item.type}</p>
       <p>{item.name}</p>
       <p>{item.description}</p>
       <p>{item.price / 100}</p>
+
+      <button className="menu__buttons" onClick={increment}>
+        +
+      </button>
+      {count}
+      <button className="menu__buttons" onClick={decrement}>
+        -
+      </button>
     </div>
   );
 }
---
+++ src/hooks/useCount.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export default function useCount() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    setCount(count - 1);
+  }
+  return [count, increment, decrement];
+}
