4a33a263694855bc326b1e494dd91f825fe8fa77
hellodebs <debora.behrens@outlook.com>
2021-08-13 14:45:17 +0200

    Add styling to bill page

--- src/pages/Bill.js
+++ src/pages/Bill.js
@@ -1,36 +1,27 @@
+import "./Bill.css";
+
 export default function Bill() {
   return (
-    <section>
-      <div>
-        <p>Dish example 1</p>
-      </div>
-      <div>
-        <p>Dish example 2</p>
-      </div>
-      <div>
-        <p>Dish example 3</p>
-      </div>
-      <div>
-        <p>Dish example 4</p>
-      </div>
-      <div>
-        <p>Dish example 5</p>
-      </div>
-      <div>
-        <p>Dish example 6</p>
-      </div>
-      <div>
-        <p>Dish example 7</p>
-      </div>
-      <div>
-        <p>Dish example 8</p>
-      </div>
-      <div>
-        <p>Dish example 9</p>
-      </div>
-      <div>
-        <p>Total: 64.20€</p>
-      </div>
-    </section>
+    <div className="bill__item">
+      <p>Dish example 1</p>
+
+      <p>Dish example 2</p>
+
+      <p>Dish example 3</p>
+
+      <p>Dish example 4</p>
+
+      <p>Dish example 5</p>
+
+      <p>Dish example 6</p>
+
+      <p>Dish example 7</p>
+
+      <p>Dish example 8</p>
+
+      <p>Dish example 9</p>
+
+      <p>Total: 64.20€</p>
+    </div>
   );
 }
