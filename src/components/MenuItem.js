650859736390b5991ba84c34731fb9f4b552f305
hellodebs <debora.behrens@outlook.com>
2021-08-13 10:24:47 +0200

    Correct css class and padding for counter button

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,9 +1,11 @@
 import "./MenuItem.css";
 import useCount from "../hooks/useCount";
 import { BiMinus, BiPlus } from "react-icons/bi";
+import { useState } from "react";
 
 export default function MenuItem({ item }) {
   const [count, increment, decrement] = useCount();
+  const [orderItem, setOrderItem] = useState();
   return (
     <div className="menu__item">
       <p>{item.type}</p>
@@ -11,10 +13,17 @@ export default function MenuItem({ item }) {
       <p>{item.description}</p>
       <p>{item.price / 100}</p>
       <div className="menu__counter">
-        <BiPlus className="menu__counter--buttons" onClick={increment} />
+        <BiPlus
+          className="menu__counter--button"
+          onClick={increment}
+          onSubmit={handleSubmit}
+        />
         <span className="menu__counter--number">{count}</span>
-        <BiMinus className="menu__counter--buttons" onClick={decrement} />
+        <BiMinus className="menu__counter--button" onClick={decrement} />
       </div>
     </div>
   );
+  function handleSubmit() {
+    setOrderItem(orderItem);
+  }
 }
--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,33 +1,5 @@
+import MenuItem from "../components/MenuItem";
+
 export default function Order() {
-  return (
-    <section>
-      <div>
-        <p>I want dish 1</p>
-      </div>
-      <div>
-        <p>I want dish 2</p>
-      </div>
-      <div>
-        <p>I want dish 3</p>
-      </div>
-      <div>
-        <p>I want dish 4</p>
-      </div>
-      <div>
-        <p>I want dish 5</p>
-      </div>
-      <div>
-        <p>I want dish 6</p>
-      </div>
-      <div>
-        <p>I want dish 7</p>
-      </div>
-      <div>
-        <p>I want dish 8</p>
-      </div>
-      <div>
-        <p>I want dish 9</p>
-      </div>
-    </section>
-  );
+  return <section></section>;
 }
