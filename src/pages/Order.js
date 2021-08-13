c5013f7b87f5ea640a2cc880a49755d07c7799e7
hellodebs <debora.behrens@outlook.com>
2021-08-13 15:09:45 +0200

    Add delete button

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,25 +1,44 @@
 import "./Order.css";
+import { BiX } from "react-icons/bi";
 
 export default function Order() {
   return (
     <div className="order__item">
-      <p>I want dish 1</p>
+      <p>
+        I want dish 1 <BiX />
+      </p>
 
-      <p>I want dish 2</p>
+      <p>
+        I want dish 2 <BiX />
+      </p>
 
-      <p>I want dish 3</p>
+      <p>
+        I want dish 3 <BiX />
+      </p>
 
-      <p>I want dish 4</p>
+      <p>
+        I want dish 4 <BiX />
+      </p>
 
-      <p>I want dish 5</p>
+      <p>
+        I want dish 5 <BiX />
+      </p>
 
-      <p>I want dish 6</p>
+      <p>
+        I want dish 6 <BiX />
+      </p>
 
-      <p>I want dish 7</p>
+      <p>
+        I want dish 7 <BiX />
+      </p>
 
-      <p>I want dish 8</p>
+      <p>
+        I want dish 8 <BiX />
+      </p>
 
-      <p>I want dish 9</p>
+      <p>
+        I want dish 9 <BiX />
+      </p>
 
       <button type="submit" className="order__item--button">
         Confirm Order
