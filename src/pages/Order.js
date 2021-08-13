785a97e364085d9cee2bc015b59a1ec660e5c67e
hellodebs <debora.behrens@outlook.com>
2021-08-13 14:43:24 +0200

    Add styling to order page

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,3 +1,25 @@
+import "./Order.css";
+
 export default function Order() {
-  return <section></section>;
+  return (
+    <div className="order__item">
+      <p>I want dish 1</p>
+
+      <p>I want dish 2</p>
+
+      <p>I want dish 3</p>
+
+      <p>I want dish 4</p>
+
+      <p>I want dish 5</p>
+
+      <p>I want dish 6</p>
+
+      <p>I want dish 7</p>
+
+      <p>I want dish 8</p>
+
+      <p>I want dish 9</p>
+    </div>
+  );
 }
