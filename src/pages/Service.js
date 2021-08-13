b8a0e6f59b5e29989098d1ef80b86ab9776eda32
hellodebs <debora.behrens@outlook.com>
2021-08-13 14:47:09 +0200

    Add styling to service page

--- src/pages/Service.js
+++ src/pages/Service.js
@@ -1,10 +1,11 @@
+import "./Service.css";
+
 export default function Service() {
   return (
     <section>
-      <div>
+      <div className="service__text">
         <p>Thank you!</p>
-      </div>
-      <div>
+
         <p>One member of our Team will be with you shortly!</p>
       </div>
     </section>
