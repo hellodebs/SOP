5b7357707a7e96695e4aeb49d5eb71c439ae4a96
hellodebs <debora.behrens@outlook.com>
2021-08-18 16:05:00 +0200

    Display total amount of chosen food items

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -2,11 +2,13 @@ import OrderItem from "../components/OrderItem";
 import "./Order.css";
 
 export default function Order({ items, order, deleteButtonHandler }) {
+  let total = 0;
   const orderItems = order.map((orderItem) => {
     const menuItem = items.filter((item) => {
       return item.id === orderItem.id;
     });
     orderItem = { ...orderItem, ...menuItem[0] };
+    total = total + orderItem.price * orderItem.quantity;
     return (
       <OrderItem
         deleteButtonHandler={deleteButtonHandler}
@@ -15,11 +17,12 @@ export default function Order({ items, order, deleteButtonHandler }) {
       />
     );
   });
+
   return (
     <>
       {orderItems}
       <button type="submit" className="order__confirm-button">
-        Confirm order
+        Confirm order ({total / 100} €)
       </button>
     </>
   );
